edc2532829b16028347ba488c1d4d80b0a25134b
LiaPetrova <88048660+hvpetrow@users.noreply.github.com>
2024-01-14 19:38:50 +0100

    svgs created and added in folder

---
+++ public/svgs/svgs.js
@@ -0,0 +1,4 @@
+export const svg = {
+    hamburgerMenuSvg:  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 lg:hidden" fill="#686583" width="24" height="24" viewBox="0 0 24 24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" /></svg>,
+    closeSvg: <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="#686583" width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41z"/></svg>,
+}
