0f508d41101d1c6bdc45878cfc39f38e6057e889
ivomirtolev <ivomir.tolev@vuframe.com>
2024-01-14 10:34:08 +0200

    add: project version in footer

--- ui/components/_base/Footer.js
+++ ui/components/_base/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+const { version } = require('/package.json');
+
 
 const Footer = () => {
   return (
     <footer className="text-center py-4">
       <p>
-        &copy; {new Date().getFullYear()} ICPTokens<br />
+        &copy; {new Date().getFullYear()} ICPTokens v{version}<br />
         Powered by the <a href="https://internetcomputer.org/" className='underline'>Internet Computer</a> & <a href="https://www.tradingview.com/" className='underline'>Trading View</a>
       </p>
     </footer>
