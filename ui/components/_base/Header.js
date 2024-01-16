3cca124ea67b9c33f75670a1beb5b0000acc6f92
LiaPetrova <88048660+hvpetrow@users.noreply.github.com>
2024-01-17 00:12:47 +0100

    style updated

--- ui/components/_base/Header.js
+++ ui/components/_base/Header.js
@@ -9,6 +9,7 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
+
 import * as React from 'react';
 
 const Header = () => {
@@ -16,7 +17,6 @@ const Header = () => {
         { href: '/useful-links', label: 'Useful Links' },
         // Add more links as needed
     ];
-
     const path = useRouter().route;
     const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false);
     const [marketsIsOpen, setmarketsIsOpen] = useState(false);
@@ -52,57 +52,59 @@ const Header = () => {
                         }
                     </div>
                     {/* If hamburger-menu is clicked */}
-                    <ul className={`${hamburgerIsOpen ? 'visible animate-fadeInLeft' : 'invisible animate-fadeInRight'} absolute  z-[100] transition-visible ease-in-out delay-150 duration-300 top-14 left-0 bg-white w-full h-full shadow-xl px-4 py-8`}>
-                        {/* Before Element For Active Link */}
-                        <li className={`${path === '/currencies' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-green-500"} relative mb-4 py-2`}>
-                            <div className="relative flex gap-[0.8rem] items-center">
-                                <img src="/logo.png" alt="ICP Tokens logo" className="w-[24px] h-[24px]" />
-                                <Link href="/currencies" onClick={hamburgerMenuHandler} className={`${path === '/currencies' && "text-active-link-green"}`}>Cryptocurrencies</Link>
-                            </div>
-                        </li>
-                        <li className={`${path === '/feed' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-green-500"} relative mb-4 py-2`}>
-                            <div className="relative flex gap-[0.8rem] items-center">
-                                <img src="/logo.png" alt="ICP Tokens logo" className="w-[24px] h-[24px]" />
-                                <Link href="/feed" onClick={hamburgerMenuHandler} className={`${path === '/feed' && "text-active-link-green"}`}>Feed</Link>
-                            </div>
-                        </li>
-                        <li className={`${path === '/library' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-green-500"} relative mb-4 py-2`}>
-                            <div className="relative flex gap-[0.8rem] items-center">
-                                <img src="/logo.png" alt="ICP Tokens logo" className="w-[24px] h-[24px]" />
-                                <Link href="/library" onClick={hamburgerMenuHandler} className={`${path === '/library' && "text-active-link-green"}`}>Library</Link>
-                            </div>
-                        </li>
-                        <li className={`${path === '/markets' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-green-500"} relative mb-4 py-2`}>
-                            <div className="flex justify-between">
+                    <div className={`${hamburgerIsOpen ? 'visible animate-fadeInLeft' : 'invisible animate-fadeInRight'} flex flex-col justify-between transition-visible ease-in-out delay-150 duration-300 px-4 py-8 absolute  z-[100] top-14 left-0 bg-white w-full h-[93%] shadow-xl`} >
+                        <ul >
+                            {/* Before Element For Active Link */}
+                            <li className={`${path === '/currencies' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-green-500"} relative mb-4 py-2`}>
                                 <div className="relative flex gap-[0.8rem] items-center">
                                     <img src="/logo.png" alt="ICP Tokens logo" className="w-[24px] h-[24px]" />
-                                    <Link href="/markets" onClick={hamburgerMenuHandler} className={`${path === '/markets' && "text-active-link-green"}`}>Markets</Link>
+                                    <Link href="/currencies" onClick={hamburgerMenuHandler} className={`${path === '/currencies' && "text-active-link-green"}`}>Cryptocurrencies</Link>
                                 </div>
-
-                                <div onClick={marketButtonHandler}>
-                                    {marketsIsOpen
-                                        ? <KeyboardArrowDownIcon fontSize="large" />
-                                        : <KeyboardArrowUpIcon fontSize="large" />
-                                    }
+                            </li>
+                            <li className={`${path === '/feed' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-green-500"} relative mb-4 py-2`}>
+                                <div className="relative flex gap-[0.8rem] items-center">
+                                    <img src="/logo.png" alt="ICP Tokens logo" className="w-[24px] h-[24px]" />
+                                    <Link href="/feed" onClick={hamburgerMenuHandler} className={`${path === '/feed' && "text-active-link-green"}`}>Feed</Link>
                                 </div>
-                            </div>
-                        </li>
-                        {marketsIsOpen &&
-                            <div className="mx-4">
-                                <li className="mb-4 py-2">
-                                    <Link href="/dex" onClick={hamburgerMenuHandler} className={`${path === '/dex' && "text-active-link-green"}`}>DEX</Link>
+                            </li>
+                            <li className={`${path === '/library' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-green-500"} relative mb-4 py-2`}>
+                                <div className="relative flex gap-[0.8rem] items-center">
+                                    <img src="/logo.png" alt="ICP Tokens logo" className="w-[24px] h-[24px]" />
+                                    <Link href="/library" onClick={hamburgerMenuHandler} className={`${path === '/library' && "text-active-link-green"}`}>Library</Link>
+                                </div>
+                            </li>
+                            <li className={`${path === '/markets' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-green-500"} relative mb-4 py-2`}>
+                                <div className="flex justify-between">
+                                    <div className="relative flex gap-[0.8rem] items-center">
+                                        <img src="/logo.png" alt="ICP Tokens logo" className="w-[24px] h-[24px]" />
+                                        <Link href="/markets" onClick={hamburgerMenuHandler} className={`${path === '/markets' && "text-active-link-green"}`}>Markets</Link>
+                                    </div>
+                                    <div onClick={marketButtonHandler}>
+                                        {marketsIsOpen
+                                            ? <KeyboardArrowDownIcon fontSize="large" />
+                                            : <KeyboardArrowUpIcon fontSize="large" />
+                                        }
+                                    </div>
+                                </div>
+                            </li>
+                            {marketsIsOpen &&
+                                <div className="mx-4">
+                                    <li className="mb-4 py-2">
+                                        <Link href="/dex" onClick={hamburgerMenuHandler} className={`${path === '/dex' && "text-active-link-green"}`}>DEX</Link>
 
-                                </li>
-                                <li className="mb-4 py-2">
-                                    <Link href="/nft" onClick={hamburgerMenuHandler} className={`${path === '/nft' && "text-active-link-green"}`}>NFT</Link>
-                                </li>
-                            </div>
-                        }
-                    </ul>
-                    <div className="flex">
-                        <TwitterIcon/>
-                        <FacebookRoundedIcon/>
-                        <InstagramIcon/>
+                                    </li>
+                                    <li className="mb-4 py-2">
+                                        <Link href="/nft" onClick={hamburgerMenuHandler} className={`${path === '/nft' && "text-active-link-green"}`}>NFT</Link>
+                                    </li>
+                                </div>
+                            }
+                        </ul>
+                        {/* If hamburger-menu is clicked social media will be showed */}
+                        <div className={`${hamburgerIsOpen ? 'visible animate-fadeInLeft' : 'invisible animate-fadeInRight'} flex justify-center gap-4 transition-visible ease-in-out delay-150 duration-300`}>
+                            <TwitterIcon sx={{ fontSize: 38 }} />
+                            <FacebookRoundedIcon sx={{ fontSize: 38 }} />
+                            <InstagramIcon sx={{ fontSize: 38 }} />
+                        </div>
                     </div>
 
                     {/* Desktop */}
