7173d7e712a56d8f674d1deade5ee80b8d1fdc8f
LiaPetrova <88048660+hvpetrow@users.noreply.github.com>
2024-01-17 00:13:03 +0100

    font-awesome imports

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,10 @@ export default function App({ Component, pageProps }) {
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/icon?family=Material+Icons"
                 />
+                <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-
+awesome.min.css" rel="stylesheet" integrity="sha384-
+wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" 
+crossorigin="anonymous"></link>
                 {/* Other head elements */}
             </Head>
             <Component {...pageProps} />
