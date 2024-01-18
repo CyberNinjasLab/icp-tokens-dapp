27911564d9d488d5a4d03c58f56bf3e4aa6fc50c
ivomirtolev <ivomir.tolev@vuframe.com>
2024-01-18 18:33:14 +0200

    add: dynamic chart period change

--- pages/token/[symbol].js
+++ pages/token/[symbol].js
@@ -3,18 +3,10 @@ import ChartComponent from '../../ui/components/ChartComponent';
 import Layout from '../../ui/components/_base/Layout';
 import { testData } from '../table/seedData';
 import TokenDetailsInfo from '../../ui/components/TokenDetailsInfo';
-import { Button, ButtonGroup } from '@mui/material';
 
 const TokenPage = () => {
   const router = useRouter();
   const { symbol } = router.query; // Access the "symbol" parameter
-
-  const buttons = [
-    <Button key="1d">1D</Button>,
-    <Button key="1w">1W</Button>,
-    <Button key="1m">1M</Button>,
-    <Button key="1y">1Y</Button>
-  ];
 
   return (
     <Layout>
@@ -27,14 +19,6 @@ const TokenPage = () => {
         </div>
         <h1 className="text-lg font-semibold">$9,999</h1>
         <ChartComponent />
-        <ButtonGroup
-          variant="outlined"
-          aria-label="outlined primary button group"
-          fullWidth={true}
-          className="my-3"
-        >
-          {buttons}
-        </ButtonGroup>
         <TokenDetailsInfo />
       </div>
     </Layout>
