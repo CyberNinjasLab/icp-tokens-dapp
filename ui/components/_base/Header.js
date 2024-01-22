6771a54c3861840694f00953886c821994416b1a
LiaPetrova <88048660+hvpetrow@users.noreply.github.com>
2024-01-22 21:37:08 +0100

    navigation desktop/mobile implemented

--- ui/components/_base/Header.js
+++ ui/components/_base/Header.js
@@ -1,37 +1,230 @@
+import * as React from 'react';
 import Link from 'next/link';
-import Logo from './Logo';
+import { useState } from 'react';
+import { useRouter } from 'next/router'
+import { useRef } from "react"
+
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import RedditIcon from '@mui/icons-material/Reddit';
+import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
+import FeedIcon from '@mui/icons-material/Feed';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+import { MenuList, Paper } from '@mui/material';
+
+import useOnClickOutside from '../../hooks/clickOutsideHook';
 
 const Header = () => {
-  const navLinks = [
-    { href: '/markets', label: 'Markets' },
-    { href: '/useful-links', label: 'Useful Links' },
-    // Add more links as need
-  ];
+	const marketsMenuWrapperRef = useRef(null);
+	const accountMenuWrapperRef = useRef(null);
 
-  return (
-    <header className="mx-auto container px-4 md:px-8 py-4">
-      <div className="flex items-center space-x-12">
-        <div className="flex items-center space-x-4">
-          {/* Use Link without <a> */}
-          <Link href="/">
-            <Logo />
-          </Link>
-        </div>
-        <nav>
-          <ul className="flex space-x-12">
-            {navLinks.map(link => (
-              <li key={link.href}>
-                {/* Use Link without <a> */}
-                <Link href={link.href}>
-                  <span className="cursor-pointer">{link.label}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+	const path = useRouter().route;
+	const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false);
+	const [marketsIsOpenMobile, setmarketsIsOpenMobile] = useState(false);
+	const [marketsIsOpenDesktop, setmarketsIsOpenDesktop] = useState(false);
+	const [accountIsOpen, setAccountIsOpen] = useState(false);
+	useOnClickOutside(marketsMenuWrapperRef, () => setmarketsIsOpenDesktop(false));
+	useOnClickOutside(accountMenuWrapperRef, () => setAccountIsOpen(false));
+
+	const hamburgerMenuHandler = () => {
+		setHamburgerIsOpen(!hamburgerIsOpen);
+	};
+
+	const marketButtonHandlerMobile = () => {
+		setmarketsIsOpenMobile(!marketsIsOpenMobile);
+	};
+
+	const marketButtonHandlerDesktop = () => {
+		setmarketsIsOpenDesktop(!marketsIsOpenDesktop);
+	};
+
+	const accountButtonHandler = () => {
+		setAccountIsOpen(!accountIsOpen);
+	};
+
+	const navLinks = [
+		{ href: '/currencies', label: 'Cryptocurrencies' },
+		{ href: '/feed', label: 'Feed' },
+		{ href: '/library', label: 'Library' },
+		// Add more links as needed
+	];
+
+	return (
+		<header className="border-b border-solid py-4 px-4 overflow-visible lg:px-8">
+			<div className="flex justify-between lg:justify-start items-center lg:flex-start lg:space-x-12">
+				<div className="flex items-center space-x-4">
+					{/* Use Link without <a> */}
+					<Link href="/">
+						<span className="flex items-center space-x-4 cursor-pointer">
+							<img src="/logo.png" alt="ICP Tokens logo" className="w-[35px]" />
+							<span className="font-semibold uppercase tracking-wide">
+								ICP<span className="text-token-title-green">Tokens</span>
+							</span>
+						</span>
+					</Link>
+				</div>
+				<nav>
+					{/* {navLinks.map(link => 
+                                <li key={link.href} className={`lg:after:w-0 hover:after:w-full lg:hover:after:bg-active-link-green after:m-auto lg:relative after:content-[''] after:rounded-sm after:block after:h-1 after:bg-transparent after:transition-all after:duration-300 ${path === '/currencies' && "lg:after:w-full lg:after:bg-active-link-green"}`}>
+                                    <div className="relative flex gap-[0.8rem] items-center">
+                                        <Link href={link.href} className={`${path === link.href ? "text-active-link-green" : "text-mobile-menu-grey"}`}>{link.label}  {path}   {link.href}</Link>
+                                    </div>
+                                </li>                          
+                        )} */}
+					{/* Desktop - links */}
+					<ul className="hidden lg:flex lg:items-center lg:space-x-4 lg:gap-4">
+						<li className={`lg:after:w-0 hover:after:w-full lg:hover:after:bg-active-link-green after:m-auto lg:relative after:content-[''] after:rounded-sm after:block after:h-1 after:bg-transparent after:transition-all after:duration-300 ${path === '/currencies' && "lg:after:w-full lg:after:bg-active-link-green"}`}>
+							<div className="relative flex gap-[0.8rem] items-center">
+								<Link href="/currencies" className={`${path === '/currencies' && "text-active-link-green"} text-mobile-menu-grey`}>Cryptocurrencies</Link>
+							</div>
+						</li>
+						<li className={`lg:after:w-0 hover:after:w-full lg:hover:after:bg-active-link-green after:m-auto lg:relative after:content-[''] after:rounded-sm after:block after:h-1 after:bg-transparent after:transition-all after:duration-300 ${path === '/feed' && "lg:after:w-full lg:after:bg-active-link-green"}`}>
+							<div className="relative flex gap-[0.8rem] items-center">
+								<Link href="/feed" className={`${path === '/feed' && "text-active-link-green"} text-mobile-menu-grey`}>Feed</Link>
+							</div>
+						</li>
+						<li className={`lg:after:w-0 hover:after:w-full lg:hover:after:bg-active-link-green after:m-auto lg:relative after:content-[''] after:rounded-sm after:block after:h-1 after:bg-transparent after:transition-all after:duration-300 ${path === '/library' && "lg:after:w-full lg:after:bg-active-link-green"}`}>
+							<div className="relative flex gap-[0.8rem] items-center">
+								<Link href="/library" className={`${path === '/library' && "text-active-link-green"} text-mobile-menu-grey`}>Library</Link>
+							</div>
+						</li>
+						<li className={`lg:after:w-0 hover:after:w-full lg:hover:after:bg-active-link-green after:m-auto lg:relative after:content-[''] after:rounded-sm after:block after:h-1 after:bg-transparent after:transition-all after:duration-300 ${(path === '/dex' || path === '/nft') && "lg:after:w-full lg:after:bg-active-link-green"}`}>
+							<div className="flex justify-between">
+								<div className="relative flex gap-[0.8rem] items-center">
+									<button className="text-mobile-menu-grey">Markets</button>
+								</div>
+								<div onClick={marketButtonHandlerDesktop}>
+									{marketsIsOpenDesktop
+										? <KeyboardArrowUpIcon className="text-mobile-menu-grey cursor-pointer" sx={{ fontSize: 24, position: 'absolute' }} />
+										: <KeyboardArrowDownIcon className="text-mobile-menu-grey cursor-pointer" sx={{ fontSize: 24, position: 'absolute' }} />
+									}
+								</div>
+							</div>
+							{marketsIsOpenDesktop &&
+								<div className="lg:absolute lg:top-9 lg:w-[60px] lg:text-center">
+									<Paper ref={marketsMenuWrapperRef}>
+										<MenuList>
+											<li className="py-2">
+												<Link href="/dex" className={`${path === '/dex' ? "text-active-link-green" : "text-mobile-menu-grey"}`}>DEX</Link>
+											</li>
+											<li className="py-2">
+												<Link href="/nft" className={`${path === '/nft' ? "text-active-link-green" : "text-mobile-menu-grey"}`}>NFT</Link>
+											</li>
+										</MenuList>
+									</Paper>
+								</div>
+							}
+						</li>
+					</ul>
+
+					{/* Mobile */}
+					<div onClick={hamburgerMenuHandler} className="lg:hidden">
+						{hamburgerIsOpen
+							? <CloseIcon className="text-mobile-menu-grey" fontSize="large" />
+							: <MenuIcon className="text-mobile-menu-grey" fontSize="large" />
+						}
+					</div>
+					{/* If hamburger-menu is clicked */}
+					<div className={`${hamburgerIsOpen ? 'visible animate-fadeInLeft' : 'invisible animate-fadeInRight'} flex flex-col justify-between transition-visible ease-in-out delay-150 duration-300 px-4 py-8 absolute  z-[100] top-14 left-0 bg-white w-full h-[93%] shadow-xl`} >
+						<ul >
+							{/* Before Element For Active Link */}
+							<li className={`${path === '/currencies' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-active-link-green"} relative mb-4 py-2`}>
+								<div className="relative flex gap-[0.8rem] items-center">
+									<AllInclusiveIcon className={`${path === '/currencies' ? "text-active-link-green" : "text-mobile-menu-grey"}`} fontSize="large" />
+									<Link href="/currencies" onClick={hamburgerMenuHandler} className={`${path === '/currencies' ? "text-active-link-green" : "text-mobile-menu-grey"}`}>Cryptocurrencies</Link>
+								</div>
+							</li>
+							<li className={`${path === '/feed' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-active-link-green"} relative mb-4 py-2`}>
+								<div className="relative flex gap-[0.8rem] items-center">
+									<FeedIcon className={`${path === '/feed' ? "text-active-link-green" : "text-mobile-menu-grey"}`} fontSize="large" />
+									<Link href="/feed" onClick={hamburgerMenuHandler} className={`${path === '/feed' ? "text-active-link-green" : "text-mobile-menu-grey"}`}>Feed</Link>
+								</div>
+							</li>
+							<li className={`${path === '/library' && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-active-link-green"} relative mb-4 py-2`}>
+								<div className="relative flex gap-[0.8rem] items-center">
+									<MenuBookIcon className={`${path === '/library' ? "text-active-link-green" : "text-mobile-menu-grey"}`} fontSize="large" />
+									<Link href="/library" onClick={hamburgerMenuHandler} className={`${path === '/library' ? "text-active-link-green" : "text-mobile-menu-grey"}`}>Library</Link>
+								</div>
+							</li>
+							<li className={`${(path === '/dex' || path === '/nft') && "before:content-[''] before:absolute before:-left-4 before:top-0 before:h-full before:border-l-4 before:border-solid before:border-active-link-green"} relative mb-4 py-2`}>
+								<div className="flex justify-between">
+									<div className="relative flex gap-[0.8rem] items-center">
+										<SwapHorizIcon className={`${(path === '/dex' || path === '/nft') ? "text-active-link-green" : "text-mobile-menu-grey"}`} fontSize="large" />
+										<button className={`${(path === '/dex' || path === '/nft') ? "text-active-link-green" : "text-mobile-menu-grey"}`}>Markets</button>
+									</div>
+									<div onClick={marketButtonHandlerMobile}>
+										{marketsIsOpenMobile
+											? <KeyboardArrowUpIcon className="text-mobile-menu-grey" sx={{ fontSize: 35 }} />
+											: <KeyboardArrowDownIcon className="text-mobile-menu-grey" sx={{ fontSize: 35 }} />
+										}
+									</div>
+								</div>
+							</li>
+							{marketsIsOpenMobile &&
+								<div className="mx-4">
+									<li className="mb-4 py-2">
+										<Link href="/dex" onClick={hamburgerMenuHandler} className={`${path === '/dex' ? "text-active-link-green" : "text-mobile-menu-grey"}`}>DEX</Link>
+									</li>
+									<li className="mb-4 py-2">
+										<Link href="/nft" onClick={hamburgerMenuHandler} className={`${path === '/nft' ? "text-active-link-green" : "text-mobile-menu-grey"}`}>NFT</Link>
+									</li>
+								</div>
+							}
+						</ul>
+						{/* If hamburger-menu is clicked social media will be showed */}
+						<div className={`${hamburgerIsOpen ? 'visible animate-fadeInLeft' : 'invisible animate-fadeInRight'} flex justify-center gap-4 transition-visible ease-in-out delay-150 duration-300`}>
+							<TwitterIcon className="text-mobile-menu-grey" sx={{ fontSize: 38 }} />
+							<FacebookRoundedIcon className="text-mobile-menu-grey" sx={{ fontSize: 38 }} />
+							<InstagramIcon className="text-mobile-menu-grey" sx={{ fontSize: 38 }} />
+							<RedditIcon className="text-mobile-menu-grey" sx={{ fontSize: 38 }} />
+						</div>
+					</div>
+				</nav>
+
+				{/* Desktop  - notification and account */}
+				<div className="hidden lg:flex lg:items-center lg:!ml-auto lg:gap-4">
+					<button>
+						<NotificationsNoneIcon className="text-mobile-menu-grey" sx={{ fontSize: 28 }} />
+					</button>
+					<div className="relative">
+						<button>
+							<AccountCircleIcon onClick={accountButtonHandler} className="text-mobile-menu-grey" sx={{ fontSize: 28 }} />
+						</button>
+						{accountIsOpen &&
+							<Paper ref={accountMenuWrapperRef} sx={{
+								position: 'absolute',
+								width: 80,
+								textAlign: 'center',
+								top: 36,
+								right: -25,
+							}}>
+								<MenuList>
+									<li className="py-2">
+										<Link href="/profile" className={`${path === '/profile' ? "text-active-link-green" : "text-mobile-menu-grey"}`}>Profile</Link>
+									</li>
+									<li className="py-2">
+										<Link href="/account-settings" className={`${path === '/accout-settings' ? "text-active-link-green" : "text-mobile-menu-grey"}`}>Settings</Link>
+									</li>
+									<li className="py-2">
+										<Link href="/logout" className="text-mobile-menu-grey">Logout</Link>
+									</li>
+								</MenuList>
+							</Paper>
+						}
+					</div>
+				</div>
+			</div>
+		</header >
+	);
 };
 
 export default Header;
