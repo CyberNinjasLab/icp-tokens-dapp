d8ea8b8b78aaff5e32723391e7380ec346e58e65
LiaPetrova <88048660+hvpetrow@users.noreply.github.com>
2024-02-08 22:31:59 +0100

    mapping and styles updated

--- pages/news.js
+++ pages/news.js
@@ -13,6 +13,14 @@ const News = () => {
 	
 	console.log(typeNews);
 
+	const news = [
+		{title:"Cryptoticker.io – Der Podcast – News Episode 3",description:"In der neuen Episode vom Cryptoticker.io Podcast besprechen wir u.a. die Beschlagnahmung von 50.000 BTC und den Jupiter Airdrop.",source:"CryptoTicker",image:"sonic.png",id:"1"},
+		{title:"Real World Assets: Mit diesen 5 Coins partizipiert man am Megatrend",description:"Die Tokenisierung von Real World Assets (RWA) ist ein Billionen-Markt. Welche fünf Kryptowährungen sich anbieten, um am Megatrend zu profitieren With hundreds of thousands of coins and toke With hundreds of thousands of coins and toke",source:"BTC-ECHO",image:"icp.png",id:"2"},
+		{title:"SEC wins motion to disclose Ripple financial statements, XRP sales details",description:"A federal judge ruled that Ripple must disclose its financial records and details of XRP sales in the SEC's lawsuit over an unregistered securitie With hundreds of thousands of coins and tokeWith hundreds of thousands of coins and toke With hundreds of thousands of coins and toke",source:"crypto.news",image:"mod.png",id:"3"},
+		{title:"Best Token for Instant 20x Profits in February 2024",description:"The post Best Token for Instant 20x Profits in February 2024 appeared first on Coinpedia Fintech News With hundreds of thousands of coins and tokeWith hundreds of thousands of coins and toke With hundreds of thousands of coins and toke",source:"Coinpedia Fintech News",image:"sns1.png",id:"4"},
+		{title:"Hackers switching to centralized exchanges to fund crypto attacks",description:"There is growing concern about the number of crypto hackers using centralized exchanges to fund their attacks.In order to pay the transaction fees With hundreds of thousands of coins and toke With hundreds of thousands of coins and toke",source:"Cryptonews",image:"sonic.png",id:"5"},
+	];
+
 	return (
 		<Layout>
 			<div className='flex flex-row justify-between items-center'>
@@ -41,8 +49,11 @@ const News = () => {
 					</FormControl>
 				</Box>
 			</div>
-
-			<NewsCard></NewsCard>
+			<div className="mt-16">
+			{
+				news.map(n => <NewsCard key={n.id} news={n}></NewsCard>)
+			}
+			</div>
 		</Layout>
 
 	);
--- ui/components/newsCard.js
+++ ui/components/newsCard.js
@@ -2,79 +2,44 @@
 import {Card, CardContent, CardMedia, Link, Typography } from '@mui/material';
 import React from 'react';
 
-const NewsCard = () => {
+const NewsCard = ({news}) => {
   return (
     <div>
       <Card sx={{
-        maxWidth: 800, display: 'grid', gridTemplateColumns: { md: '1fr 2fr' },
+        maxWidth: 1000, display: 'grid', gridTemplateColumns: { md: '1fr 2fr' },
         gap: 2,
         margin: "0 auto",
         variant: 'outlined',
         // border: '1px solid #000',
-        padding: "64px 8px"
+        padding: "38px 8px",
+        borderBottom: "1px dashed transperent",
       }}>
         <div className='rounded-lg'>
         <Link className="!no-underline" href={"https://google.com/"} target="_blank">
           <CardMedia
             sx={{ height: 200, borderRadius: "8px" }}
-            image="/tokens/chat.png"
-            title="green iguana"
+            image={`/tokens/${news.image}`}
+            title={news.image}
           />
           </Link>
         </div>
-        <div className='max-h-[200px] m-auto'>
-          <CardContent sx={{ padding: 1, '&:last-child': { pb: 1 } }}>
+        {/* overflow: "hidden", textOverflow:"ellipsis",whiteSpace: "nowrap" */}
+        <div className="h-[200px] flex flex-col justify-between items-start m-auto">
+          <CardContent sx={{ padding: 1, '&:last-child': { pb: 1 },}}>
             <Link className="!no-underline" href={"https://google.com/"} target="_blank">
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
+              <Typography className="line-clamp-2" gutterBottom variant="h6" component="div">
+                {news.title}
               </Typography>
             </Link>
-            <Typography variant="subtitle1" color="text.secondary" sx={{ padding: 0 }}>
-              Lizards are a widespread group of squamate reptiles, with over 6,000
-              species, ranging across all continents except Antarctica 
+            <Typography className="line-clamp-2" variant="subtitle1" color="text.secondary" sx={{ padding: 0,  }}>
+              {news.description} 
             </Typography>
           </CardContent>
           <Typography variant="overline" display="block" gutterBottom sx={{fontSize:14, padding: 1 }}>
-            Bg-mama
+            {news.source}
           </Typography>
         </div>
       </Card>
-
-      <Card sx={{
-        maxWidth: 800, display: 'grid', gridTemplateColumns: { md: '1fr 2fr' },
-        gap: 2,
-        margin: "0 auto",
-        variant: 'outlined',
-        // border: '1px solid #000',
-        padding: "64px 8px"
-      }}>
-        <div className='rounded-lg'>
-        <Link className="!no-underline" href={"https://gong.bg/"} target="_blank">
-          <CardMedia
-            sx={{ height: 200, borderRadius: "8px" }}
-            image="/tokens/mod.png"
-            title="green iguana"
-          />
-          </Link>
-        </div>
-        <div className='max-h-[200px] m-auto'>
-          <CardContent sx={{ padding: 1, '&:last-child': { pb: 1 } }}>
-            <Link className="!no-underline" href={"https://gong.bg/"} target="_blank">
-              <Typography gutterBottom variant="h5" component="div">
-                Chicken
-              </Typography>
-            </Link>
-            <Typography variant="body1" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over 6,000
-              species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000
-              species
-            </Typography>
-          </CardContent>
-          <Typography variant="overline" display="block" gutterBottom sx={{ fontSize:14 ,padding: 1 }}>
-            Bg-mama
-          </Typography>
-        </div>
-      </Card >
     </div >
   );
 };
