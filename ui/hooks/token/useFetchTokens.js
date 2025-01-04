4fac17313a2de4b8aad8a56aa315991fd4a933f3
Boyan Klisurov <boyanklisurov@gmail.com>
2025-01-04 21:04:31 +0200

    cache tokens response

--- ui/hooks/token/useFetchTokens.js
+++ ui/hooks/token/useFetchTokens.js
@@ -5,12 +5,39 @@ const useFetchTokens = (url) => {
   const [loaded, setLoaded] = useState(false);
   const [error, setError] = useState(null);
 
-  // Define the fetchData function using useCallback to ensure it doesn't change if the url doesn't
+  // Function to check if the cached data is still valid
+  const isCacheValid = (timestamp) => {
+    const now = new Date().getTime();
+    return now - timestamp < 60000; // 60000 milliseconds = 1 minute
+  };
+
+  // Fetch data from local storage or API
   const fetchData = useCallback(async () => {
     setLoaded(false); // Reset the loaded state on each fetch
+
     try {
+      const cacheKey = `cache_${url}`;
+      const cachedData = localStorage.getItem(cacheKey);
+
+      if (cachedData) {
+        const { data: cachedDataValue, timestamp } = JSON.parse(cachedData);
+        if (isCacheValid(timestamp)) {
+          setData(cachedDataValue);
+          setLoaded(true);
+          return; // Return if we use cached data
+        }
+      }
+
+      // If there's no valid cache, fetch new data
       const response = await fetch(url);
       const jsonData = await response.json();
+
+      // Save to local storage
+      localStorage.setItem(cacheKey, JSON.stringify({
+        data: jsonData,
+        timestamp: new Date().getTime()
+      }));
+
       setData(jsonData);
       setLoaded(true);
       setError(null); // Clear any previous errors on successful fetch
