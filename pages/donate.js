642fa65f16dc5d8e94d1bfbcc8372b2ad5ba91a6
Boyan Klisurov <boyanklisurov@gmail.com>
2025-01-09 21:50:03 +0200

    donation page

--- pages/donate.js
+++ pages/donate.js
@@ -14,11 +14,11 @@ const Donations = () => {
 								Your help matters ❤️
 								</span>
                 <div className='mt-8 '>
-                    <div className='border-gray-500 bg-gray-900/60 border shadow-xl rounded-xl inline-block px-4 py-6 lg:px-10 lg:py-8 min-w-[440px]'>
+                    <div className='border-gray-500 bg-gray-900/60 border shadow-xl rounded-xl inline-block px-8 py-6 lg:px-10 lg:py-8 w-full sm:w-auto sm:min-w-[440px]'>
                         <ToggleButtonGroup
                             value={toggleValue}
                             exclusive
-                            onChange={(event, currentSelection) => setToggleValue(currentSelection)}
+                            onChange={(event, currentSelection) => setToggleValue(toggleValue)}
                             color="primary"
                             sx={{
                                 gap: '10px',
@@ -40,28 +40,16 @@ const Donations = () => {
                                     ICP
                                 </ToggleButton>
                             </Tooltip>
-                            {/* <Tooltip title="ckBTC Token">
-                                <ToggleButton value="ckBTC">
-                                    <img src="/tokens/ckBTC.png" className='w-6 h-6 mr-2' />
-                                    ckBTC
-                                </ToggleButton>
-                            </Tooltip>
-                            <Tooltip title="ckUSDC Token">
-                                <ToggleButton value="ckUSDC">
-                                    <img src="/tokens/ckUSDC.svg" className='w-6 h-6 mr-2' />
-                                    ckUSDC
-                                </ToggleButton>
-                            </Tooltip> */}
                         </ToggleButtonGroup>
                         <div className='mt-6 text-center mb-4'>
                             <div>
                                 <img src={isICP ? "/qr/AID.png" : "/qr/PID.png"} className='w-52 h-52 inline-block' />
                                 <div className='mt-3'>
-                                    <span className='relative inline-block break-words leading-4 max-w-[270px]'>
+                                    <span className='relative inline-block break-words leading-4 max-w-[210px] pr-8 sm:max-w-[270px]'>
                                         {isICP 
                                             ? "67d3e28f5bd25c79ca6e2e39acc43831794def163cbe0475e03a59b56e97b933"
                                             : "djeau-aivho-uyrm3-wqglm-oxb4y-imkyo-cus4h-fi3hw-alswa-wcv7l-bae"}
-                                        <span className='inline-block absolute left-full top-1/2 transform -translate-y-1/2 ml-2'>
+                                        <span className='inline-block absolute right-1 top-1/2 transform -translate-y-1/2 ml-2'>
                                             <CopyButton value={isICP ? "67d3e28f5bd25c79ca6e2e39acc43831794def163cbe0475e03a59b56e97b933" : "djeau-aivho-uyrm3-wqglm-oxb4y-imkyo-cus4h-fi3hw-alswa-wcv7l-bae"} />
                                         </span>	
                                     </span>
--- ui/components/_base/DonateBanner.js
+++ ui/components/_base/DonateBanner.js
@@ -15,28 +15,32 @@ const DonateBanner = () => {
   }
 
   return (
-    <div className='fixed z-50 bottom-3 left-1/2 -translate-x-1/2 transform px-4 leading-normal lg:w-auto max-w-[580px]'>
-      <div className="bg-dark-bg border w-full border-gray-500 shadow-xl rounded-xl p-3 flex items-center justify-between">
-        <div className='pr-4'>  
+    <div className='fixed z-50 bottom-3 left-1/2 -translate-x-1/2 transform px-4 leading-normal lg:w-auto w-full xs:w-[450px]'>
+      <div className="bg-dark-bg border w-full border-gray-500 shadow-xl justify-center items-center rounded-xl p-2 px-3 flex">
+        <div className='pr-4 flex-grow flex  text-left'>
           Support our mission and drive the vision!
         </div>
-        <Button
-          variant="contained"
-          color="primary"
-          href="/donate"
-          disableElevation
-          style={{
-            paddingLeft: "10px",
-            paddingRight: "10px",
-            paddingTop: "4px",
-            paddingBottom: "4px",
-          }}
-        >
-          Donate
-        </Button>
-        <IconButton onClick={handleClose} aria-label="close">
-          <CloseIcon style={{fontSize:16}} />
-        </IconButton>
+        <div className='flex justify-center'>
+        <div className='flex justify-center'>
+          <Button
+            variant="contained"
+            color="primary"
+            href="/donate"
+            disableElevation
+            style={{
+              paddingLeft: "10px",
+              paddingRight: "10px",
+              paddingTop: "4px",
+              paddingBottom: "4px",
+            }}
+          >
+            Donate
+          </Button>
+          <IconButton onClick={handleClose} aria-label="close" className='relative left-1'>
+            <CloseIcon style={{fontSize:16}} />
+          </IconButton>
+        </div>
+        </div>
       </div>
     </div>
   );
