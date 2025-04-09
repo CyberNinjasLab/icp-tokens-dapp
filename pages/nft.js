47bea4669400d59fccf5a003d9144ac200d7d23d
Boyan Klisurov <boyanklisurov@gmail.com>
2025-04-09 19:02:52 +0300

    wip

--- pages/nft.js
+++ pages/nft.js
@@ -12,11 +12,11 @@ const Nft = () => {
       </Head>
 			<Layout>
 				<div className='w-full'>
-					<h1 className='h1'>NFT</h1>
-					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 mt-4">
+					<h1 className='h1 text-center'>NFT</h1>
+					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-10 mt-4 max-w-[950px] mx-auto">
 						<a href="https://entrepot.app/" target='_blank' className="max-w-sm mx-auto p-6  shadow-lg dark:bg-gray-500/20 rounded-lg">
 							<div className="flex items-center space-x-4">
-								<img src="/logos/entrepot_logo.webp" alt="Logo" className="h-12 w-12" />
+								<img src="/logos/entrepot_logo.webp" alt="Entrepot Logo" className="h-12 w-12" />
 								<h2 className="font-bold text-lg">Entrepot</h2>
 							</div>
 							<div className="mt-4">
@@ -25,7 +25,7 @@ const Nft = () => {
 						</a>
 						<a href="https://bioniq.io/" target='_blank' className="max-w-sm mx-auto p-6  shadow-lg dark:bg-gray-500/20 rounded-lg">
 							<div className="flex items-center space-x-4">
-								<img src="/logos/bioniq-logo.jpeg" alt="Logo" className="h-12 w-12" />
+								<img src="/logos/bioniq-logo.jpeg" alt="Bioniq Logo" className="h-12 w-12" />
 								<h2 className="font-bold text-lg">Bioniq</h2>
 							</div>
 							<div className="mt-4">
@@ -34,7 +34,7 @@ const Nft = () => {
 						</a>
 						<a href="https://yumi.io/" target='_blank' className="max-w-sm mx-auto p-6  shadow-lg dark:bg-gray-500/20 rounded-lg">
 							<div className="flex items-center space-x-4">
-								<img src="/logos/yumi_logo.webp" alt="Logo" className="h-12 w-12" />
+								<img src="/logos/yumi_logo.webp" alt="Yumi NFT Market Logo" className="h-12 w-12" />
 								<h2 className="font-bold text-lg">Yumi</h2>
 							</div>
 							<div className="mt-4">
@@ -43,11 +43,20 @@ const Nft = () => {
 						</a>
 						<a href="https://yuku.app/" target="_blank" className="max-w-sm mx-auto p-6  shadow-lg dark:bg-gray-500/20 rounded-lg">
 							<div className="flex items-center space-x-4">
-								<img src="/logos/yuku_logo.png" alt="Logo" className="h-12 w-12" />
+								<img src="/logos/yuku_logo.png" alt="YUKU NFT Market Logo" className="h-12 w-12" />
 								<h2 className="font-bold text-lg">Yuku</h2>
 							</div>
 							<div className="mt-4">
 								<p>This AI Web3 Platform integrates an NFT Aggregator, 3D space, and AI avatars, pioneering a new dimension in digital interactions.</p>
+							</div>
+						</a>
+						<a href="https://dgdg.app/" target="_blank" className="max-w-sm mx-auto p-6  shadow-lg dark:bg-gray-500/20 rounded-lg">
+							<div className="flex items-center space-x-4">
+								<img src="/logos/dgdg-nft.webp" alt="Multi-chain NFT Aggregator Logo" className="h-12 w-12" />
+								<h2 className="font-bold text-lg">DGDG</h2>
+							</div>
+							<div className="mt-4">
+								<p>A multi-chain NFT aggregator built for the potatoverse. Effortlessly explore NFT collections on the Internet Computer, sort and filter by key metrics.</p>
 							</div>
 						</a>
 					</div>
